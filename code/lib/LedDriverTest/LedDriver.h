2730e6c3b61171ef38ae8a8b8c056542a3e1b9b9
Ben Wang <benwang@umich.edu>
2014-10-09 14:32:10 -0400

    Started PCB schematic

--- code/lib/LedDriverTest/LedDriver.h
+++ code/lib/LedDriverTest/LedDriver.h
@@ -5,12 +5,13 @@
 #include <arduino.h>
 
 const unsigned int LED_CLK = 2;	
-const unsigned int LED_LE  = 3;	
-const unsigned int LED_OE  = 4;		// Can always tie to low for LEDs to always be enabled
-const unsigned int LED_SDI = 5;	
+const unsigned int LED_LE  = 7;	
+const unsigned int LED_OE  = 3;		// Can always tie to low for LEDs to always be enabled
+const unsigned int LED_SDI = 4;	
 
 void LED_initialize();
 void LED_write(char data);
+void LED_setBrightness(int brightness);
 void LED_enableOutput();
 void LED_disableOutput();
 
@@ -34,17 +35,24 @@ void LED_write(char data){
 	// Write LED data bits
 	for (int led = 0; led < 8; ++led){
 		// Write a bit
-		digitalWrite(LED_LE, (data & 0x1));
+		digitalWrite(LED_SDI, (data & 0x80));
 		digitalWrite(LED_CLK, 1);
 		digitalWrite(LED_CLK, 0);
 		
 		// Shift data
-		data = data >> 1;
+		data = data << 1;
 	}
+
+        // Set SDI wire to low
+        digitalWrite(LED_SDI, 0);
 	
 	// Latch data
 	digitalWrite(LED_LE, 1);
 	digitalWrite(LED_LE, 0);
+}
+
+void LED_setBrightness(int brightness){
+	analogWrite(LED_OE, 255 - brightness);  // "255 - " Because active LOW
 }
 
 void LED_enableOutput(){
---
+++ code/lib/LedDriverTest/LedDriverTest.ino
@@ -0,0 +1,127 @@
+// Sample program using TLC5917IN LED driver
+
+#include "LedDriver.h"
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Initialize LEDs
+  LED_initialize();
+  
+  // Set all off
+  LED_write(0xA7);
+  
+  // Enable
+  LED_enableOutput();
+  
+  // Wait and show debug status
+  pinMode(13, OUTPUT);
+  digitalWrite(13, 1);  // Debug status LED
+  delay(100);
+  digitalWrite(13,0);
+  
+  // Disable
+  LED_disableOutput();
+  
+  // Pulse all LEDs to indicate transition
+  pulseAll();
+}
+
+void loop() {
+  
+//********************************************************************
+//********** Turn all LEDs on, ramp up then down *********************
+//********************************************************************
+  Serial.println("Ramp up");
+  
+  // Set LEDs on
+  LED_write(0xAA);
+  
+  // Ramp up and down brightness
+  for (int j = 0; j < 256; ++j){
+    LED_setBrightness(j);
+    delay(5);
+    
+    Serial.print("  "); Serial.println(j);
+  }
+  
+  // Switch LEDs
+  LED_write(0x55);
+  
+  Serial.println("Ramp down");
+  for (int j = 255; j >= 0; --j){
+    LED_setBrightness(j);
+    delay(5);
+    
+    Serial.print("  "); Serial.println(j);
+  }
+  
+  LED_disableOutput();
+  
+  // Transition
+  delay(100);
+  
+//********************************************************************
+//********** Light up individual LEDs ********************************
+//********************************************************************
+  
+  // Light each LED individually
+  for (int j = 0; j < 8; ++j){
+Serial.print("Lighting LED: "); Serial.println(j);
+    LED_write(1 << j);
+    LED_enableOutput();
+    delay(300);
+  }
+  
+  LED_disableOutput();
+  
+  // Transition
+  delay(100);
+  
+//********************************************************************
+//**** Binary counter Demo *******************************************
+//********************************************************************
+
+  // Enable LEDs throughout entire 
+  LED_enableOutput();
+
+  // Light each LED individually
+  for (int j = 0; j < 128; ++j){
+Serial.print("  Writing to LEDs: "); Serial.println((int)j);
+    LED_write(j);
+    delay(5);
+  }
+  
+  LED_disableOutput();
+  
+  // Transition
+  delay(100);
+}
+
+
+
+
+
+
+//********************************************************************
+//********** Pulse all LEDs to indicate transistion ******************
+//********************************************************************
+
+void pulseAll(){
+  Serial.print("Pulsing");
+  
+  // Set all on
+  LED_write(0xff);
+  
+  // Enable
+  LED_enableOutput();
+  
+  // Wait and show debug status
+  digitalWrite(13, 1);  // Debug status LED
+  delay(100);
+  digitalWrite(13,0);
+  
+  // Disable
+  LED_disableOutput();
+}
+//#include "LedDriver.cpp.h"
